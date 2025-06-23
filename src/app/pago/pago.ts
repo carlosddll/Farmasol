3c6c2f572287237a94b35c2b979b574068e18992
Carlos Díaz Llontop <48460815+carlosddll@users.noreply.github.com>
2025-06-23 18:55:37 -0500

    Add files via upload

--- src/app/pago/pago.ts
+++ src/app/pago/pago.ts
@@ -1,7 +1,8 @@
 import { NgFor } from '@angular/common';
 import { CommonModule } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
+import { Component, inject, NgModule } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
+import { CarritoService } from '../services/carrito';
 
 
 
@@ -12,11 +13,9 @@ import { FormsModule, NgModel } from '@angular/forms';
   styleUrl: './pago.css'
 })
 export class Pago {
-
-  carrito = [
-    { nombre: 'Vitamina C', precio: 25.9, cantidad: 1 },
-    { nombre: 'Omega 3', precio: 39.9, cantidad: 2 },
-  ];
+  // Variable publica para acceder desde pago.html
+  // inyectar el servicio carritoService
+  public carritoService = inject(CarritoService); 
 
   pago = {
     nombre: '',
@@ -25,6 +24,27 @@ export class Pago {
     cvv: '',
     guardar: false
   };
+
+  // Accedemos al arreglo del carrito usando el servicio.
+  get carrito() {
+    return this.carritoService.getCarrito(); // Devuelve el arreglo de productos desde el servicio
+  }
+
+  // Calculamos el total
+  get total(): number {
+    return this.carrito.reduce((acc, item) => acc + item.producto.precio * item.cantidad, 0);
+  }
+
+  // proceso de pago
+  pagar() {
+    console.log('Procesando pago:', this.pago);                
+    alert('¡Pago procesado correctamente!');          
+  }
+/*
+  carrito = [
+    { nombre: 'Vitamina C', precio: 25.9, cantidad: 1 },
+    { nombre: 'Omega 3', precio: 39.9, cantidad: 2 },
+  ];
 
   get total(): number {
     return this.carrito.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
@@ -35,5 +55,5 @@ export class Pago {
     alert('¡Pago procesado correctamente!');
     // Aquí llamarías a tu servicio backend o pasarela real
   }
-
+*/
 }
